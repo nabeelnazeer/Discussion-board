8d927cf34de32c32da7263b0b89097c17627693b
Nabeel-nazeer <nabeelnazeer2565@gmail.com>
2023-06-22 09:58:31 +0300

    updated views with ne template but basic idea

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -8,10 +8,5 @@ from boards.models import Board
 
 def home(request):
     boards = Board.objects.all()
-    board_names =  list()
-    for board in boards:
-        board_names.append(board.name)
 
-    response_html = '<br>'.join(board_names)
-
-    return HttpResponse(response_html)    
+    return render(request, 'home.html', {'boards':boards})    
